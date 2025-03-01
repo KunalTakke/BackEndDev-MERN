e163620271483efcb439870c1270e7e2001c3643
KunalTakke <kunal.technophilic@gmail.com>
2025-03-01 23:53:47 +0530

    Added Dom Challenge Files

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -4,3 +4,9 @@ ul.innerHTML = "Kunal";
 var color = document.querySelector(".list a");
 console.log(color);
 color.style.color = "Red";
+
+
+var h1 = document.querySelector("h1");
+console.log(h1);
+
+h1.classList.add("big");
